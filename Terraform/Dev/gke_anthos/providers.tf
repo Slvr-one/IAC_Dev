c9bbcbe8a300cf9079abca83c1aafaf43954dee3
dvir gross <dvirgross2000@gmail.com>
2023-07-08 01:51:21 +0300

    mod Terraform/Dev/gke_anthos/providers.tf

--- Terraform/Dev/gke_anthos/providers.tf
+++ Terraform/Dev/gke_anthos/providers.tf
@@ -11,3 +11,18 @@ provider "google" {
   project     = var.project_id
   region      = var.region
 }
+
+provider "google-beta" {
+  project     = var.project_id
+  region      = var.region
+}
+
+provider "external" {
+  version = "2"
+}
+
+provider "template" {
+  version = ""
+}
+
+
